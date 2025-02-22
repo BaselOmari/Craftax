38295076a67e645e5ba1cb306d33cfff3d329b1b
Bassel Al Omari <b2alomar@uwaterloo.ca>
2025-02-22 20:32:56 +0000

    update score compute

--- craftax/craftax_marl/envs/common.py
+++ craftax/craftax_marl/envs/common.py
@@ -1,13 +1,21 @@
-from craftax_marl.craftax_state import EnvState
+from typing import List
+
+from craftax_marl.craftax_state import EnvState, StaticEnvParams
 from craftax_marl.constants import *
 
 
-def compute_score(state: EnvState, done: bool):
+def compute_score(state: EnvState, done: bool, player_names: List, static_params: StaticEnvParams):
     achievements = state.achievements * done * 100.0
     info = {}
     for achievement in Achievement:
-        name = f"Achievements/{achievement.name.lower()}"
-        info[name] = achievements[achievement.value]
+        achievement_name = f"Achievements/{achievement.name.lower()}"
+        player_total = jnp.array(0.0)
+        for player in range(static_params.player_count):
+            player_achievement_name = f"{achievement_name}/{player_names[player]}"
+            player_achievement_value = achievements[player, achievement.value]
+            info[player_achievement_name] = player_achievement_value
+            player_total = jnp.maximum(player_total, player_achievement_value)
+        info[achievement_name] = player_total
     # Geometric mean with an offset of 1%
     info["score"] = jnp.exp(jnp.mean(jnp.log(1 + achievements))) - 1.0
     return info
--- craftax/craftax_marl/envs/craftax_symbolic_env.py
+++ craftax/craftax_marl/envs/craftax_symbolic_env.py
@@ -41,7 +41,7 @@ class CraftaxMARLSymbolicEnv(MultiAgentEnv):
 
         obs = self.get_obs(state)
         done = self.is_terminal(state, self.default_params)
-        info = compute_score(state, done)
+        info = compute_score(state, done, self.agents, self.static_env_params)
         info["discount"] = self.discount(state, self.default_params)
 
         agent_rewards = {n: r for n,r in zip(self.agents, reward)}
